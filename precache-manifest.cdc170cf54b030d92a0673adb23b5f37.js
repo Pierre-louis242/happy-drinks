27a8765f40d8c10a0a13c4aaca4ecd9515ea7000
Pierre-Louis Picard <Pierre-Louis@Ioanas-MacBook-Air.local>
2019-04-18 10:29:26 +0200

    Updates

---
+++ precache-manifest.cdc170cf54b030d92a0673adb23b5f37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/happydrinks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "260a8a6d512f458b3e11",
+    "url": "/happydrinks/static/js/runtime~main.0178c2fa.js"
+  },
+  {
+    "revision": "4ad85cb85b90eff5ed98",
+    "url": "/happydrinks/static/js/main.332c4ac8.chunk.js"
+  },
+  {
+    "revision": "57067a6506edfda2d571",
+    "url": "/happydrinks/static/js/2.0250be17.chunk.js"
+  },
+  {
+    "revision": "4ad85cb85b90eff5ed98",
+    "url": "/happydrinks/static/css/main.e1e779d8.chunk.css"
+  },
+  {
+    "revision": "c486297b9a922f2083c4d444ac3a7dcc",
+    "url": "/happydrinks/index.html"
+  }
+];
